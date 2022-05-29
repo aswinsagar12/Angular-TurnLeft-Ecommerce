6796aaa081fd1a888ee25f679e5ba2415d825a08
aswinsagar12 <aswinsagar12@gmail.com>
2022-05-29 14:03:41 +0530

    Cart Page Added

--- turnleft-frontend/src/app/services/product.service.ts
+++ turnleft-frontend/src/app/services/product.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+import { ProductModelServer, ServerResponse } from '../models/products.model';
 
 @Injectable({
   providedIn: 'root',
@@ -10,10 +12,18 @@ export class ProductService {
   constructor(private http: HttpClient) {}
 
   //fetch all product from backend
-  getAllProducts(numberOfResults: number = 10) {
-    return this.http.get(this.SERVER_URL + '/products', {
+  getAllProducts(numberOfResults: number = 10): Observable<ServerResponse> {
+    return this.http.get<ServerResponse>(this.SERVER_URL + '/products', {
       params: { limit: numberOfResults.toString() },
     });
+  }
+
+  //get single product from backend
+
+  getSingleProduct(id: number): Observable<ProductModelServer> {
+    return this.http.get<ProductModelServer>(
+      this.SERVER_URL + '/products' + id
+    );
   }
 
   showMessage() {
